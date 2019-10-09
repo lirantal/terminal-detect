41bda111f8d2695599f6443d981f06eae3a13742
Liran Tal <liran.tal@gmail.com>
2019-10-09 23:17:04 +0300

    feat(detectors): add unicode and color detection

--- bin/terminal-detect.js
+++ bin/terminal-detect.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
 'use strict'
+
+const util = require('util')
+const supportsColor = require('supports-color')
+const hasUnicode = require('has-unicode')
+
+const properties = {
+  colors: {
+    16: supportsColor.stdout.hasBasic,
+    256: supportsColor.stdout.has256,
+    '16m': supportsColor.stdout.has16m
+  },
+  charset: {
+    unicode: !!hasUnicode()
+  }
+}
+
+console.log(util.inspect(properties, false, null, false))
